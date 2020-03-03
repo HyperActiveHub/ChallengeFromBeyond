e430ea17838fdab56d8c919a52309ad8f85d11e3
Claes Svensson <Claes2000@outlook.com>
2020-03-03 23:30:49 +0100

    Start menu

---
+++ Assets/Scripts/Menu/MenuButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButton : MonoBehaviour
+{
+	[Tooltip("Most likely the canvas")] [SerializeField] MenuButtonController menuButtonController;
+	[SerializeField] Animator animator;
+	[SerializeField] AnimatorButtonFunctions animatorButtonFunctions;
+	[SerializeField] int thisIndex;
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (menuButtonController.index == thisIndex)
+		{
+			animator.SetBool("selected", true);
+			if (Input.GetAxis("Submit") == 1)
+			{
+				animator.SetBool("pressed", true);
+			}
+			else if (animator.GetBool("pressed"))
+			{
+				animator.SetBool("pressed", false);
+				animatorButtonFunctions.disableOnce = true;
+			}
+		}
+		else
+		{
+			animator.SetBool("selected", false);
+		}
+	}
+}
