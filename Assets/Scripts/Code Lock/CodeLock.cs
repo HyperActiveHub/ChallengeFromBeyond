b171241ec2ac6fd2704ce36313d8f6ebaf3dde6a
Axel Nyström Svensson <axel.nystrom-svensson@hotmail.com>
2020-03-10 12:34:17 +0100

    Codelock behaviour implemented
    Graphics and what should happen on win condition is yet to be added.

---
+++ Assets/Scripts/Code Lock/CodeLock.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CodeLock : MonoBehaviour
+{
+    [SerializeField] private List<CodeLockDigit> digits;
+    [SerializeField] private int[] solution;
+    [HideInInspector] public UnityEvent onUpdate;
+
+    private void OnEnable()
+    {
+        onUpdate.AddListener(CheckWinCondition);
+    }
+
+    private void OnDisable()
+    {
+        onUpdate.RemoveListener(CheckWinCondition);
+    }
+
+    private void CheckWinCondition()
+    {
+        if (ValidateSolution()) {
+            Debug.Log("Win");
+        }
+    }
+
+    private bool ValidateSolution()
+    {
+        if(digits.Count == 0)
+        {
+            return false;
+        }
+
+        for (int i = 0; i < digits.Count; i++)
+        {
+            if(digits[i].currentDigit != solution[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ Assets/Scripts/Code Lock/CodeLockDigit.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class CodeLockDigit : MonoBehaviour
+{
+    [SerializeField] private CodeLock codeLockReference = null;
+    [SerializeField] private int index = -1;
+    [Tooltip("What sprites to use (in order 0-9)")]
+    [SerializeField] private List<Sprite> sprites;
+
+    public int currentDigit = 0;
+    private Image imageComponent = null;
+
+    private void OnValidate()
+    {
+        if(codeLockReference == null)
+        {
+            Debug.LogWarning("Code lock not attached to this code lock number.", this);
+        }
+
+        if(imageComponent == null)
+        {
+            imageComponent = GetComponent<Image>();
+        }
+    }
+
+    private void OnEnable()
+    {
+        codeLockReference.onUpdate.AddListener(UpdateGraphic);
+    }
+
+    private void OnDisable()
+    {
+        codeLockReference.onUpdate.RemoveListener(UpdateGraphic);
+    }
+
+    private void UpdateGraphic()
+    {
+        imageComponent.sprite = sprites[currentDigit];
+    }
+
+    public void Increment()
+    {
+        currentDigit = (currentDigit + 1) % 10;
+        codeLockReference.onUpdate.Invoke();
+    }
+
+    public void Decrease()
+    {
+        currentDigit = (currentDigit - 1);
+        if (currentDigit < 0)
+        {
+            currentDigit = 9;
+        }
+        codeLockReference.onUpdate.Invoke();
+    }
+}
