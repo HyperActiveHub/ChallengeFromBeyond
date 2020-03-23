ace15998cf5691f700bea9aa2aedfbfc6e903a4a
Karim Chanane <coolkimch@gmail.com>
2020-03-23 17:04:01 +0100

    Added ConditionMet function to doors. Needs to be called before use.

--- Assets/Scripts/DoorScript.cs
+++ Assets/Scripts/DoorScript.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class DoorScript : MonoBehaviour
 {
+    bool conditionMet;
+
     public void ChangeScene(string sceneName)
     {
-        GameManagerScript.Instance.ChangeScene(sceneName);
+        if (conditionMet)
+            GameManagerScript.Instance.ChangeScene(sceneName);
+        else
+        {
+            string say = "I think I've missed something...";
+            var interact = GetComponent<InteractableObject>();
+            interact.inspectText = say;
+            interact.InspectDialog();
+        }
+    }
+
+    public void ConditionMet()
+    {
+        conditionMet = true;
     }
 }
