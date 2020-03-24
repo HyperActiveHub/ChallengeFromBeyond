f4306e863d2039d2e2b38a410149b1b393037ba0
Claes Svensson <Claes2000@outlook.com>
2020-03-24 19:11:38 +0100

    Improved movement command

--- Assets/Scripts/Fungus/PlayerMovementSwitch.cs
+++ Assets/Scripts/Fungus/PlayerMovementSwitch.cs
@@ -6,34 +6,17 @@ using Fungus;
 [CommandInfo("Player", "Switch movement", "Switches the player's movement speed to bool, other values are optional")]
 public class PlayerMovementSwitch : Command
 {
-    [Tooltip("Attach this for faster loading (optional)")] public GameManagerScript managerScript = null;
-    [Tooltip("If it's not GM")] public string Name = null;
     public bool switchTo;
-    // Start is called before the first frame update
-    void Start()
-    {
-        if (managerScript == null)
-        {
-            if (name == null)
-            {
-                managerScript = GameObject.Find("GM").GetComponent<GameManagerScript>();
-            }
-            else
-            {
-                managerScript = GameObject.Find(name).GetComponent<GameManagerScript>();
-            }
-        }
-    }
 
     public override void OnEnter()
     {
         if (switchTo)
         {
-            managerScript.SetPlayerMovement(true);
+            GameManagerScript.Instance.SetPlayerMovement(true);
         }
         else
         {
-            managerScript.SetPlayerMovement(false);
+            GameManagerScript.Instance.SetPlayerMovement(false);
         }
         Continue();
     }
